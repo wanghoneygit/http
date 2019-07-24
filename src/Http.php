ce037faf4ff650e88430d500325c242195546aec
wuwangwang <wuwangwang@dxy.cn>
2019-07-24 15:30:22 +0800

    增加测试代码

---
+++ src/Http.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wang
+ * Date: 2019/7/23
+ * Time: 下午9:33
+ */
+
+namespace Wanghoneygit\Http;
+
+class Http {
+
+    public function getTest() {
+        echo 'Hello World';
+    }
+}
